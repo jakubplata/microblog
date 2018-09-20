2bf29d8208aa50ac8adbeef46e2494648558428a
jakubplata <plata.jakub@gmail.com>
2018-09-20 17:18:31 +0200

    Dates using moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_moment import Moment
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -15,6 +16,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
