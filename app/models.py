24c14afb11026cdcc65ed795e697ad23dd289600
jakubplata <plata.jakub@gmail.com>
2018-09-23 08:24:53 +0200

    Post translations

--- app/models.py
+++ app/models.py
@@ -78,12 +78,12 @@ class User(UserMixin, db.Model):
         return followed.union(own).order_by(Post.timestamp.desc())
 
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
--- config.py
+++ config.py
@@ -15,3 +15,4 @@ class Config(object):
     ADMINS = ['plata.jakub@gmail.com']
     POSTS_PER_PAGE = 25
     LANGUAGES = ['en', 'pl']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
